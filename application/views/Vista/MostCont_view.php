a10390db8cf6842988e6fb0c4ba498ca33439217
leninr <lronquillo@udlanet.ec>
2017-01-14 16:24:00 -0500

    mostrar Contenido

--- application/views/Vista/MostCont_view.php
+++ application/views/Vista/MostCont_view.php
@@ -1,54 +1,269 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Contenido</title>
-</head>
-<body>
+<!doctype html>
+	<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<link rel="icon" type="image/png" href="http://localhost/Proyecto/assets/img/favicon.ico">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-	<?php echo form_open_multipart('/Login_ctrl/logout');?>
+		<title>Get2Know!</title>
 
-    	
-    	<?= form_submit('','Log Out')?>
-		<?= form_close() ?>
+		<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+	    <meta name="viewport" content="width=device-width" />
 
-	<table style="border: 1px solid #000;width: 80%;overflow: auto">
-		<thead>
-				<tr>
-					<th>idContenido</th>
-					<th>idusuario</th>
-					<th>idcategori</th>
-					<th>nom</th>
-					<th>fecha</th>
-					<th>descrip</th>
 
-				</tr>
+	    <!-- Bootstrap core CSS     -->
+			<link rel="stylesheet" href="http://localhost/Proyecto/assets/css/bootstrap.min.css"); />
 
-		</thead>
-		<tbody>
-				<?php
-					foreach ($this->b->getalldata() as $row) {
-				?>
-					<tr>
-						<td><?php echo $row->idcontent ?></td>
-						<td><?php echo $row->idusuario ?></td>
-						<td><?php echo $row->idcategoria ?></td>
-						<td><?php echo $row->nombreCont ?></td>
-						<td><?php echo $row->fechaCont ?></td>
-						<td><?php echo $row->descripCont ?></td>
-						<td><img style="width: 150px;height: 150px;" onclick="window.location='<?php echo site_url("content_ctrl/MostrarContenidoFiltrado/".$row->idcontent);?>'"
-						src="<?php echo base_url('file/'.$row->Cont) ?>"></td>
-						<td>
-							<a href="<?php echo site_url('content_ctrl/edit/'.$row->idcontent); ?>">Edit</a>
 
-							<a href="<?php echo site_url('content_ctrl/borrarContC/'.$row->idcontent); ?>">Delete</a>
-						</td>
-					</tr>
-				<?php
-					}
-				?>
+	    <!-- Animation library for notifications   -->
+			<link rel="stylesheet" href="http://localhost/Proyecto/assets/css/animate.min.css"); />
 
-		</tbody>
-	</table>
+	    <!--  Light Bootstrap Table core CSS    -->
+			<link rel="stylesheet" href="http://localhost/Proyecto/assets/css/light-bootstrap-dashboard.css"); />
 
-</body>
-</html>
+
+	    <!--  CSS for Demo Purpose, don't include it in your project     -->
+			<link rel="stylesheet" href="http://localhost/Proyecto/assets/css/demo.css"); />
+
+
+	    <!--     Fonts and icons     -->
+	    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+			<link rel="stylesheet" href="http://localhost/Proyecto/assets/css/pe-icon-7-stroke.css"); />
+
+	</head>
+	<body>
+
+	<div class="wrapper">
+	    <div class="sidebar" data-color="purple" data-image="http://localhost/Proyecto/assets/img/sidebar-5.jpg">
+
+	    <!--
+
+	        Tip 1: you can change the color of the sidebar using: data-color="blue | azure | green | orange | red | purple"
+	        Tip 2: you can also add an image using data-image tag
+
+	    -->
+
+	    	<div class="sidebar-wrapper">
+	            <div class="logo">
+	                <a href="http://www.creative-tim.com" class="simple-text">
+	                    <i class="fa fa-eye"></i>
+	                </a>
+	            </div>
+							<div>
+									<div class="card card-user">
+											<div class="image">
+													<img src="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&fm=jpg&h=300&q=75&w=400" alt="..."/>
+											</div>
+											<div class="content">
+													<div class="author">
+															 <a href="#">
+															<br>
+															<br>
+															<br>
+															<br>
+															<br>
+															<br>
+
+																<h4 class="title"><?php echo $this->session->userdata('Nombre');?>  <?php echo $this->session->userdata('Apellido');?><br />
+
+																	 <small><?php echo $this->session->userdata('username');?></small>
+
+
+																</h4>
+															</a>
+													</div>
+													<p class="description text-center"> <?php echo $this->session->userdata('sobre');?>
+													</p>
+											</div>
+											<hr>
+											<div class="text-center">
+													<button href="#" class="btn btn-simple"><i class="fa fa-user"></i></button>
+
+													<?= form_open("/user_ctrl/EditarUsuarioC/".$this->session->userdata('idusuario')); ?>
+													<button type="submit" class="btn btn-simple"> <i class="fa fa-gears"></i></button>
+													<?= form_close() ?>
+													<button href="#" class="btn btn-simple"><i class="fa fa-trophy"></i></button>
+
+											</div>
+									</div>
+							</div>
+	    	</div>
+	    </div>
+
+	    <div class="main-panel">
+	        <nav class="navbar navbar-default navbar-fixed">
+	            <div class="container-fluid">
+	                <div class="navbar-header">
+	                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example-2">
+	                        <span class="sr-only">Toggle navigation</span>
+	                        <span class="icon-bar"></span>
+	                        <span class="icon-bar"></span>
+	                        <span class="icon-bar"></span>
+	                    </button>
+	                    <a class="navbar-brand" href="http://localhost/Proyecto/content_ctrl/MostrarContenido">Get2Know!</a>
+	                </div>
+	                <div class="collapse navbar-collapse">
+
+	                    <ul class="nav navbar-nav navbar-right">
+	                        <li class="dropdown">
+	                              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+	                                    Mi Arte
+	                                    <b class="caret"></b>
+	                              </a>
+	                              <ul class="dropdown-menu">
+	                                <li><a href="#">Ver Mis Artes</a></li>
+	                                <li><a href="#">Agregar Nueva Arte</a></li>
+	                              </ul>
+	                        </li>
+	                        <li>
+																	<?php echo form_open_multipart('/Login_ctrl/logout');?>
+
+																    	<?= form_submit('','Log Out',"class='btn btn-outline btn-link btn-sm'")?>
+																		<?= form_close() ?>
+
+	                        </li>
+	                    </ul>
+	                </div>
+	            </div>
+	        </nav>
+
+
+	        <div class="content">
+	            <!-- Página Hija-->
+
+							<div class="container-fluid">
+	                <div class="row">
+	                    <div class="col-md-12">
+	                        <div class="card">
+	                            <div class="header">
+	                                <h4 class="title" style="color:purple">Muro</h4>
+	                            </div>
+	                            <div class="content table-responsive table-full-width">
+	                                <table class="table table-hover table-striped">
+	                                    <thead>
+																					<th></th>
+																					<th>Usuario</th>
+																					<th>Categoría</th>
+																					<th>Nombre de Arte</th>
+																					<th>Subido El</th>
+																					<th>Descripción</th>
+	                                    </thead>
+	                                    <tbody>
+																				<?php
+																					foreach ($this->b->getalldata() as $row) {
+																				?>
+																					<tr>
+
+																						<td><img style="width: 150px;height: 150px;" onclick="window.location='<?php echo site_url("content_ctrl/MostrarContenidoFiltrado/".$row->idcontent);?>'"
+																						src="<?php echo base_url('file/'.$row->Cont) ?>"></td>
+																						<td><?php foreach ($this->u->findById($row->idusuario) as $users) {
+																						 					echo $users->UsuarioName;
+																										} ?></td>
+																						<td><?php foreach ($this->c->findById($row->idcategoria) as $categories) {
+																						 					echo $categories->NomCategoria;
+																										} ?></td>
+																						<td><?php echo $row->nombreCont ?></td>
+																						<td><?php echo $row->fechaCont ?></td>
+																						<td><?php echo $row->descripCont ?></td>
+																						<!--<td>
+																							<a href="<?php echo site_url('content_ctrl/edit/'.$row->idcontent); ?>">Editar</a>
+																						</td>
+																						<td>
+																							<a href="<?php echo site_url('content_ctrl/borrarContC/'.$row->idcontent); ?>" style="color:red">Eliminar</a>
+																						</td>-->
+																					</tr>
+																				<?php
+																					}
+																				?>
+
+	                                    </tbody>
+	                                </table>
+	                            </div>
+	                        </div>
+	                    </div>
+	                </div>
+	            </div>
+	        </div>
+
+
+	        <footer class="footer">
+	            <div class="container-fluid">
+	                <nav class="pull-left">
+	                    <ul>
+	                        <li>
+	                            <a href="#">
+	                                Home
+	                            </a>
+	                        </li>
+	                        <li>
+	                            <a href="#">
+	                                Company
+	                            </a>
+	                        </li>
+	                        <li>
+	                            <a href="#">
+	                                Portfolio
+	                            </a>
+	                        </li>
+	                        <li>
+	                            <a href="#">
+	                               Blog
+	                            </a>
+	                        </li>
+	                    </ul>
+	                </nav>
+	                <p class="copyright pull-right">
+	                    &copy; 2016 <a href="http://www.creative-tim.com">Creative Tim</a>, made with love for a better web
+	                </p>
+	            </div>
+	        </footer>
+
+	    </div>
+	</div>
+
+
+	</body>
+
+	    <!--   Core JS Files   -->
+
+			<script type="text/javascript" src="http://localhost/Proyecto/assets/js/jQuery-1.10.2.js");></script>
+
+		<script type="text/javascript" src="http://localhost/Proyecto/assets/js/bootstrap.min.js");></script>
+
+		<!--  Checkbox, Radio & Switch Plugins -->
+		<script type="text/javascript" src="http://localhost/Proyecto/assets/js/bootstrap-checkbox-radio-switch.js");></script>
+
+		<!--  Charts Plugin -->
+		<script type="text/javascript" src="http://localhost/Proyecto/assets/js/chartist.min.js");></script>
+
+	    <!--  Notifications Plugin    -->
+			<script type="text/javascript" src="http://localhost/Proyecto/assets/js/bootstrap-notify.js");></script>
+
+	    <!--  Google Maps Plugin    -->
+	    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+
+	    <!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
+		<script type="text/javascript" src="http://localhost/Proyecto/assets/js/light-bootstrap-dashboard.js");</script>
+
+		<!-- Light Bootstrap Table DEMO methods, don't include it in your project! -->
+		<script type="text/javascript" src="http://localhost/Proyecto/assets/js/demo.js");></script>
+
+		<script type="text/javascript">
+	    	$(document).ready(function(){
+
+	        	demo.initChartist();
+
+	        	$.notify({
+	            	icon: 'pe-7s-gift',
+	            	message: "Welcome to <b>Light Bootstrap Dashboard</b> - a beautiful freebie for every web developer."
+
+	            },{
+	                type: 'info',
+	                timer: 4000
+	            });
+
+	    	});
+		</script>
+
+	</html>
