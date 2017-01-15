04fa6afe425edf86f08881c11d1ef70959a4f5b8
leninr <lronquillo@udlanet.ec>
2017-01-15 00:29:37 -0500

    Actualiza Contenido
    Eso y unos detallitos mas :dancers:

--- application/views/Vista/UpdateContenido_view.php
+++ application/views/Vista/UpdateContenido_view.php
@@ -1,35 +1,292 @@
-<!DOCTYPE html>
-<head>
-<title>Contenido :D</title>
-</head>
-<body>
+	<!doctype html>
+	<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<link rel="icon" type="image/png" href="http://localhost/Proyecto/assets/img/favicon.ico">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-<div>
+		<title>Get2Know!</title>
 
-<?php echo form_open_multipart('/content_ctrl/do_edit2');?>
-	
-	<?= form_input(array('name' => 'idcontent', 'value' => $Conte->result()[0]->idcontent,'placeholder' => 'id Contenido')) ?> <a> id contenido</a>
-	<br>
+		<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+	    <meta name="viewport" content="width=device-width" />
 
-	<?= form_input(array('name' => 'idusuario', 'value' => $Conte->result()[0]->idusuario,'placeholder' => 'id usuario')) ?> <a>id usuario</a>
-	<br>
-	<?= form_input(array('name' => 'idcategoria', 'value' => $Conte->result()[0]->idcategoria,'placeholder' => 'id categoria')) ?> <a> id categoria</a>
-	<br>
-	<?= form_input(array('name' => 'nombreCont', 'value' => $Conte->result()[0]->nombreCont,'placeholder' => 'nombre Contenido')) ?><a>nombre contenido</a>
-	<br>
-	<?= form_input(array('name' => 'fechaCont', 'value' => $Conte->result()[0]->fechaCont,'placeholder' => 'nombre usuario')) ?><a>fecha Contenido</a>
-	<br>	
-	<?= form_input(array('name' => 'descripCont', 'value' => $Conte->result()[0]->descripCont,'placeholder' => 'nombre usuario')) ?><a>descripcion</a>
-	<br>	
-	<?= form_input(array('name' => 'descripCont', 'value' => $Conte->result()[0]->Calificación,'placeholder' => 'nombre usuario')) ?><a>calficiacion</a>
-	<br>
-	<img style="width: 150px;height: 150px;" src="<?php echo base_url('file/'.$Conte->result()[0]->Cont) ?>">
-	<?= form_submit('','Actualizar Contenido')?>
-	<?= form_close() ?>
 
-	
+	    <!-- Bootstrap core CSS     -->
+			<link rel="stylesheet" href="http://localhost/Proyecto/assets/css/bootstrap.min.css"); />
 
-</form>
 
-</body>
-</html>
+	    <!-- Animation library for notifications   -->
+			<link rel="stylesheet" href="http://localhost/Proyecto/assets/css/animate.min.css"); />
+
+	    <!--  Light Bootstrap Table core CSS    -->
+			<link rel="stylesheet" href="http://localhost/Proyecto/assets/css/light-bootstrap-dashboard.css"); />
+
+
+	    <!--  CSS for Demo Purpose, don't include it in your project     -->
+			<link rel="stylesheet" href="http://localhost/Proyecto/assets/css/demo.css"); />
+
+
+	    <!--     Fonts and icons     -->
+	    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+			<link rel="stylesheet" href="http://localhost/Proyecto/assets/css/pe-icon-7-stroke.css"); />
+
+	</head>
+	<body>
+
+	<div class="wrapper">
+	    <div class="sidebar" data-color="purple" data-image="http://localhost/Proyecto/assets/img/sidebar-5.jpg">
+
+	    <!--
+
+	        Tip 1: you can change the color of the sidebar using: data-color="blue | azure | green | orange | red | purple"
+	        Tip 2: you can also add an image using data-image tag
+
+	    -->
+
+	    	<div class="sidebar-wrapper">
+	            <div class="logo">
+	                <a href="http://localhost/Proyecto/content_ctrl/MostrarContenido" class="simple-text">
+	                    <i class="fa fa-eye"></i>
+	                </a>
+	            </div>
+							<div>
+								<div class="card card-user">
+									<?php
+											if($this->session->userdata('is_logged_in'))
+											{ ?>
+											<div class="image">
+													<img src="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&fm=jpg&h=300&q=75&w=400" alt="..."/>
+											</div>
+											<div class="content">
+													<div class="author">
+															 <a href="#">
+															<br>
+															<br>
+															<br>
+															<br>
+															<br>
+															<br>
+
+																<h4 class="title"><?php echo $this->session->userdata('Nombre');?>  <?php echo $this->session->userdata('Apellido');?><br />
+
+																	 <small><?php echo $this->session->userdata('username');?></small>
+
+
+																</h4>
+															</a>
+													</div>
+													<p class="description text-center"> <?php echo $this->session->userdata('sobre');?>
+													</p>
+											</div>
+											<hr>
+											<div class="text-center">
+													<button href="#" class="btn btn-simple"><i class="fa fa-user"></i></button>
+
+													<?= form_open("/user_ctrl/EditarUsuarioC/".$this->session->userdata('idusuario')); ?>
+													<button type="submit" class="btn btn-simple"> <i class="fa fa-gears"></i></button>
+													<?= form_close() ?>
+													<button href="#" class="btn btn-simple"><i class="fa fa-trophy"></i></button>
+
+											</div>
+									<?php } ?>
+
+								</div>
+							</div>
+	    	</div>
+	    </div>
+
+	    <div class="main-panel">
+	        <nav class="navbar navbar-default navbar-fixed">
+	            <div class="container-fluid">
+	                <div class="navbar-header">
+	                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example-2">
+	                        <span class="sr-only">Toggle navigation</span>
+	                        <span class="icon-bar"></span>
+	                        <span class="icon-bar"></span>
+	                        <span class="icon-bar"></span>
+	                    </button>
+	                    <a class="navbar-brand" href="http://localhost/Proyecto/content_ctrl/MostrarContenido">Get2Know!</a>
+	                </div>
+	                <div class="collapse navbar-collapse">
+
+										<ul class="nav navbar-nav navbar-right">
+											<?php
+													if($this->session->userdata('is_logged_in'))
+													{ ?>
+	                        <li class="dropdown">
+	                              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+	                                    Mi Arte
+	                                    <b class="caret"></b>
+	                              </a>
+	                              <ul class="dropdown-menu">
+	                                <li><a href="#">Ver Mis Artes</a></li>
+	                                <li><a href="http://localhost/Proyecto/content_ctrl/">Agregar Nueva Arte</a></li>
+	                              </ul>
+	                        </li>
+	                        <li>
+																	<?php echo form_open_multipart('/Login_ctrl/logout');?>
+
+																    	<?= form_submit('','Log Out',"class='btn btn-outline btn-link btn-sm'")?>
+																		<?= form_close() ?>
+
+	                        </li>
+											<?php } else { ?>
+
+												<li>
+																<?php echo form_open_multipart('/Login_ctrl');?>
+
+																		<?= form_submit('','Login',"class='btn btn-lg btn-success btn-block btn-sm'")?>
+																	<?= form_close() ?>
+
+												</li>
+												<?php } ?>
+
+										</ul>
+	                </div>
+	            </div>
+	        </nav>
+
+
+	        <div class="content">
+	            <!-- Página Hija-->
+
+							<?php echo form_open_multipart('/content_ctrl/do_edit2');?>
+
+	            <div class="container-fluid">
+		                <div class="row">
+		                    <div class="col-md-8">
+		                        <div class="card">
+		                            <div class="header">
+		                                <h4 class="title" style="color:purple">Actualizar Contenido</h4>
+		                            </div>
+		                            <div class="content">
+		                                <form>
+		                                    <div class="row">
+
+																						<div class="col-md-3">
+																								<div class="form-group">
+																									<label>Nombre Contenido</label>
+																									<?= form_input(array('name' => 'nombreCont','class' => 'form-control'	 , 'value' => $Conte->result()[0]->nombreCont,'placeholder' => 'nombre Contenido')) ?>
+																								</div>
+																						</div>
+		                                        <div class="col-md-3">
+		                                            <div class="form-group">
+																									<label>Categoría</label>
+																									<select class="form-control" name="idcategoria">
+																										<option value="" disabled selected>Categoria</option>
+																										<?php foreach ($this->c->obtenerCategoriaParaDropdownM() as $categories): ?>
+																											<?php if ($categories->idCategoria == $Conte->result()[0]->idcategoria): ?>
+																												<option value=".<?php echo $categories->idCategoria ?>." selected="selected"><?php echo $categories->NomCategoria ?></option>
+																											<?php else: ?>
+																													<option value=".<?php echo $categories->idCategoria ?>."> <?php echo $categories->NomCategoria ?> </option>
+																											<?php endif; ?>
+																										<?php endforeach; ?>
+																									</select>
+		                                            </div>
+		                                        </div>
+		                                        <div class="col-md-11">
+		                                            <div class="form-group">
+																									<label>Descripción</label>
+																										<?= form_input(array('name' => 'descripCont','class' => 'form-control', 'value' => $Conte->result()[0]->descripCont,'placeholder' => 'Descripcion')) ?>
+		                                            </div>
+		                                        </div>
+																						<div class="col-md-5">
+																								<div class="form-group">
+																											<img style="width: 150px;height: 150px;" src="<?php echo base_url('file/'.$Conte->result()[0]->Cont) ?>">
+																								</div>
+																						</div>
+
+		                                    </div>
+
+		                                    </div>
+
+
+																				<?= form_submit('','Actualizar Contenido',"class='btn btn-info btn-fill pull-right'")?>
+		                                    <div class="clearfix"></div>
+		                                </form>
+																		<?= form_input(array('name' => 'idcontent','type'=>'hidden' ,'value' => $Conte->result()[0]->idcontent,'placeholder' => 'id Contenido')) ?>
+																		<?= form_close() ?>
+		                            </div>
+		                        </div>
+		                    </div>
+
+											</div>
+									</div>
+
+		        </div>
+
+
+	        </div>
+
+
+	        <footer class="footer">
+	            <div class="container-fluid">
+	                <nav class="pull-left">
+	                    <ul>
+	                        <li>
+	                            <a href="http://localhost/Proyecto/content_ctrl/MostrarContenido">
+	                                Home
+	                            </a>
+	                        </li>
+	                        <li>
+	                            <a href="http://www.udla.edu.ec">
+	                                Company
+	                            </a>
+	                        </li>
+	                    </ul>
+	                </nav>
+	                <p class="copyright pull-right">
+	                    &copy; 2017 <a href="http://www.udla.edu.ec">Universidad de las Américas</a> - Ingeniería en Sistemas de Computación e Informática
+	                </p>
+	            </div>
+	        </footer>
+
+	    </div>
+	</div>
+
+
+	</body>
+
+	    <!--   Core JS Files   -->
+
+			<script type="text/javascript" src="http://localhost/Proyecto/assets/js/jQuery-1.10.2.js");></script>
+
+		<script type="text/javascript" src="http://localhost/Proyecto/assets/js/bootstrap.min.js");></script>
+
+		<!--  Checkbox, Radio & Switch Plugins -->
+		<script type="text/javascript" src="http://localhost/Proyecto/assets/js/bootstrap-checkbox-radio-switch.js");></script>
+
+		<!--  Charts Plugin -->
+		<script type="text/javascript" src="http://localhost/Proyecto/assets/js/chartist.min.js");></script>
+
+	    <!--  Notifications Plugin    -->
+			<script type="text/javascript" src="http://localhost/Proyecto/assets/js/bootstrap-notify.js");></script>
+
+	    <!--  Google Maps Plugin    -->
+	    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+
+	    <!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
+		<script type="text/javascript" src="http://localhost/Proyecto/assets/js/light-bootstrap-dashboard.js");</script>
+
+		<!-- Light Bootstrap Table DEMO methods, don't include it in your project! -->
+		<script type="text/javascript" src="http://localhost/Proyecto/assets/js/demo.js");></script>
+
+		<script type="text/javascript">
+	    	$(document).ready(function(){
+
+	        	demo.initChartist();
+
+	        	$.notify({
+	            	icon: 'pe-7s-gift',
+	            	message: "Welcome to <b>Light Bootstrap Dashboard</b> - a beautiful freebie for every web developer."
+
+	            },{
+	                type: 'info',
+	                timer: 4000
+	            });
+
+	    	});
+		</script>
+
+	</html>
